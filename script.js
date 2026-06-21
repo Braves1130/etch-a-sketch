7493601d26c5e349ae9f201cbc96de00348d1662
Michael Brockmann <289411457+Braves1130@users.noreply.github.com>
2026-06-21 18:09:47 +0200

    Add dynamic grid and resizing

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const container = document.querySelector("#container");
+const newGridBtn = document.querySelector("#new-grid-btn");
 
-function createGrid() {
-    for (let i = 0; i <256; i++) {
+newGridBtn.addEventListener("click", () => {
+const size = prompt("Wieviele quadrate nebeneinander? (Max 100)");
+
+if (size >100) {
+    altert("Bitte eine Zahl unter 100 eingeben!");
+} else{ 
+    container.innerHTML ="";
+    createGrid(size)
+}
+});
+
+
+
+function createGrid(size) {
+    const squareSize = 960 / size;
+
+    for (let i = 0; i < size * size ; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.style.width = squareSize + "px";
+        square.style.height = squareSize + "px";
         container.appendChild(square);
-            square.addEventListener("mouseover", () => {
+
+        square.addEventListener("mouseover", () => {
                 square.classList.add("squareChange");
             });
     }
 }
 
-createGrid();
